43c6fb8cade1d807f37c61088eb1d3af82bd7d2b
lalith-ae30 <lalithae30@gmail.com>
2024-11-12 22:59:51 +0530

    update image preview, and add grayscale and upscaler component

--- server/index.js
+++ server/index.js
@@ -8,69 +8,55 @@ const port = process.env.PORT || 3000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(join(__dirname, '../client/dist')));
 
+function pollJob(job, resolve, reject) {
+    prodia.getJob({ jobId: job })
+        .then(({ data }) => {
+            if (data.status === 'succeeded') {
+                resolve(data);
+            } else if (data.status === 'failed') {
+                reject(data);
+            } else {
+                setTimeout(() => pollJob(job, resolve, reject), 5000);
+            }
+        })
+        .catch(err => reject(err));
+}
+
+function handleJob(job, res) {
+    console.log(job);
+    new Promise((resolve, reject) => {
+        pollJob(job, resolve, reject);
+    })
+        .then(data => res.json({ imageUrl: data.imageUrl }))
+        .catch(err => res.status(500).json({ error: err.message, reason: "Polling" }));
+}
+
 prodia.auth('009f6d76-fa52-4c23-b785-a93b6654e31d');
 app.get('/api/generate-image', (req, res) => {
     const text = req.query.prompt || 'a rainy evening in the city';
-    let job;
     console.log('Generating image with prompt:', text);
     prodia.generate({ prompt: text })
-        .then(({ data }) => job = data.job)
+        .then(({ data }) => handleJob(data.job, res))
         .catch(err => res.status(500).json({ error: err.message, reason: "Enqueue" }));
-    const checkJobStatus = setInterval(() => {
-        prodia.getJob({ jobId: job })
-            .then(({ data }) => {
-                if (data.status === 'succeeded') {
-                    clearInterval(checkJobStatus);
-                    res.json({ imageUrl: data.imageUrl });
-                }
-            })
-            .catch(err => {
-                clearInterval(checkJobStatus);
-                res.status(500).json({ error: err.message });
-            });
-    }, 5000);
-    res.on('close', () => {
-        clearInterval(checkJobStatus);
-    });
 });
 
 app.post('/api/img2img', (req, res) => {
     let text = req.body.prompt || 'a rainy evening in the city';
     let image = req.body.image;
-    let job;
     console.log('Transforming image with prompt:', text);
     prodia.transform({ prompt: text, imageData: image })
-        .then(({ data }) => job = data.job)
-        .catch(err => {
-            res.status(500).json({ error: err.message, reason: "Enqueue" })
-            console.error(err.message);
-            console.error(image);
-            console.error(text);
-        });
-    const checkJobStatus = setInterval(() => {
-        prodia.getJob({ jobId: job })
-            .then(({ data }) => {
-                if (data.status === 'succeeded') {
-                    clearInterval(checkJobStatus);
-                    res.json({ imageUrl: data.imageUrl });
-                }
-                else if (data.status === 'failed') {
-                    clearInterval(checkJobStatus);
-                    console.error(data);
-                    console.error(image.substring(0, 100));
-                    console.error(image.substring(image.length - 100));
-                    res.status(500).json({ error: "Job failed" });
-                }
-            })
-            .catch(err => {
-                clearInterval(checkJobStatus);
-                res.status(500).json({ error: err.message });
-            });
-    }, 5000);
-    res.on('close', () => {
-        clearInterval(checkJobStatus);
-    });
+        .then(({ data }) => handleJob(data.job, res))
+        .catch(err => res.status(500).json({ error: err.message, reason: "Enqueue" })
+        );
 });
+
+app.post('/api/upscale', (req, res) => {
+    let image = req.body.image;
+    console.log('Upscaling image');
+    prodia.upscale({ resize: 2, model: 'ESRGAN_4x', imageData: image })
+        .then(({ data }) => handleJob(data.job, res))
+        .catch(err => res.status(500).json({ error: err.message, reason: "Enqueue" }));
+})
 
 app.get('*', (req, res) => {
     res.sendFile(join(__dirname, '../client/dist/index.html'));
